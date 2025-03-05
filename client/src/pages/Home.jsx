f232f2b3140bb9b96a96cf2ae962206463fb7405
Deelolade <Oluwanisholahabeeb@icloud.com>
2025-03-05 13:16:34 +0100

    done with navbar layout

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className=" bg-black">
+            <div className='bg-black text-white w-[60%] mx-auto'>
+                <h1>Hire Top Talent Today</h1>
+                <button>Start Hiring</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
